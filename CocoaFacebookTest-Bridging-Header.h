2cc7b8e02657460cf50cfd9a5bb67b349c4d95a2
Beeen <ledoux.benjamin14@gmail.com>
2016-05-05 13:22:53 +0200

    Facebook Framework

---
+++ CocoaFacebookTest-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  CocoaFacebookTest-Bridging-Header.h
+//  CocoaFacebookTest
+//
+//  Created by Benjamin Ledoux on 05/05/2016.
+//  Copyright © 2016 Benjamin Ledoux. All rights reserved.
+//
+
+#ifndef CocoaFacebookTest_Bridging_Header_h
+#define CocoaFacebookTest_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <FBSDKShareKit/FBSDKShareKit.h>
+
+
+#endif /* CocoaFacebookTest_Bridging_Header_h */
